d06d1e41e76ae381597dfe9db1daec983779ce10
gmarik <gmarik@gmail.com>
2010-03-02 18:53:39 +0200

    Bash profile.bash beautified

--- .Bash/rc.d/profile.bash
+++ .Bash/rc.d/profile.bash
@@ -1,39 +1,37 @@
-# Use bash-completion, if available
-if [ -f /etc/bash_completion ]; then
-  . /etc/bash_completion
-fi
+#
+# Completion
+#
+[ -f /etc/bash_completion ] && source /etc/bash_completion
 
+#
+# Environment
+#
 export EDITOR='vim'
 
-## Coloring grep
-#use color and line number
+#
+# Grep 
+#
 export GREP_OPTIONS='--color=auto'
-#export GREP_COLOR='0;33;47'
 
+#
 # History
-## Append each(!) history entry from all terminals realtime(not after sesion ending)
-shopt -s histappend
-##evaluated each time command line-prompt is printed so we hook up history appending there
-##if predefined PROMPT_COMMAND
-export PROMPT_COMMAND="$PROMPT_COMMAND;history -a"
-##otherwise
-#export PROMPT_COMMAND="history -a"
-## store multiline commands as 1 line
-shopt -s cmdhist
-## spelling error correction 
-shopt -s cdspell
-# check the window size after each command and, if necessary,
-# update the values of LINES and COLUMNS.
-shopt -s checkwinsize
-## ignore duplicates(store command once)
-export HISTCONTROL="ignoredups"
+#
+shopt -s histappend                         # Append each(!) history entry from all terminals realtime(not after sesion ending)
+shopt -s cmdhist                            # store multiline commands as 1 line
+shopt -s cdspell                            # spelling error correction 
+shopt -s checkwinsize                       # check the window size after each command and, if necessary, update the values of LINES and COLUMNS.
+export HISTCONTROL="ignoredups"             # ignore duplicates(store command once)
 export HISTCONTROL="ignoreboth"
-## ignore simple commands
-#export HISTIGNORE="&:ls:[bf]g:exit:%[0-9]"
-## history file size
-export HISTFILESIZE=5000
+export HISTIGNORE="&:ls:[bf]g:exit:%[0-9]"  # ignore simple commands
+export HISTFILESIZE=5000                    # history file size
 
-##Prompt string
-#PS1='\[\033[1;3;12;43m\]\D{%H:%M}\[\033[0;20m\]\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[0;35m\]$(__git_ps1 "%s\\\" )\[\033[00m\]\[\033[01;34m\]\W\[\033[00m\]\$ '
-PS1='\[\033[1;3;12;43m\]\D{%H:%M}\[\033[0;20m\]\[\033[0;35m\] $(__git_ps1 "%s\\\" )\[\033[00m\]\[\033[01;34m\]\W\[\033[00m\]\$ '
+#
+# Prompt command
+#
 export PROMPT_COMMAND='echo -ne "\033]0;${USER}@${HOSTNAME}: ${PWD/$HOME/~}\007"'
+export PROMPT_COMMAND="$PROMPT_COMMAND;history -a" #evaluated each time command line-prompt is printed so we hook up history appending there
+
+#
+# Prompt string
+#
+PS1='\[\033[1;3;12;43m\]\D{%H:%M}\[\033[0;20m\]\[\033[0;35m\] $(__git_ps1 "%s\\\" )\[\033[00m\]\[\033[01;34m\]\W\[\033[00m\]\$ '
