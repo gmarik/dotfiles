df78138cf91f8e7e49a91b9e100c1477e7156c80
gmarik <gmarik@gmail.com>
2010-03-02 19:21:57 +0200

    Bash functions merged to functions.bash

--- .bash/functions.bash
+++ .bash/functions.bash
@@ -1,3 +1,25 @@
+RCDIR=/etc/rc.d/
+
+_rc_service()
+{
+  COMPREPLY=()
+  cur="${COMP_WORDS[COMP_CWORD]}"
+  prev="${COMP_WORDS[COMP_CWORD-1]}"
+
+  local root=$RCDIR
+  local items=$(ls -1 $root)
+
+  COMPREPLY=($(compgen -W "${items}" -- "${cur}"))
+}
+
+rc()
+{
+ sudo -p 'sudo password:' $RCDIR/$1 $2
+}
+
+complete -F _rc_service rc
+
+
 RP_HOME=~/railsware/rp/
 
 _rp_dir()
@@ -22,4 +44,3 @@ complete -F _rp_dir rp
 
 ## RP
 alias cdrp="cd $RP_HOME/rp-badge-signup/"
-
