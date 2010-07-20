de1651fdb70f07b6e5e2bf2514610f5b8b7aefe0
gmarik <gmarik@gmail.com>
2010-07-19 22:50:53 -0500

    Ruby: fixed helper ActiveRecord::Base extension
    for rails 1.2.6

--- .ruby/lib/gmarik/activerecord_ext.rb
+++ .ruby/lib/gmarik/activerecord_ext.rb
@@ -1,19 +1,17 @@
 module ActiveRecord
   class Base
-    #logger = ::Logger.new(STDOUT)
-
-    class << self
+      class << self
       def first
         find(:first); 
-      end unless defined?(first)
+      end unless instance_methods.include?(:first)
 
       def last
-        find(:first, :order => 'id' ); 
-      end unless defined?(last)
+        find(:first, :order => 'id DESC' ); 
+      end unless instance_methods.include?(:last)
 
-      alias :[] :find 
-      alias :f :first 
-      alias :l :last 
-    end
+        alias  :[] :find 
+        alias  :f  :first
+        alias  :l  :last 
+      end
   end
 end if defined?(::ActiveRecord)
