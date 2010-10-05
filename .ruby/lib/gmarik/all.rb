54982fe3c10efceb5002c345fa0a76dcc336a5f5
gmarik <gmarik@gmail.com>
2010-10-05 18:35:11 -0500

    IRB: fixes

--- .ruby/lib/gmarik/all.rb
+++ .ruby/lib/gmarik/all.rb
@@ -5,5 +5,9 @@ require 'rubygems'
 require 'gmarik/utils'
 require 'gmarik/activerecord_ext'
 
+begin
 require 'interactive_editor'
 require 'gmarik/interactive_editor_ext'
+rescue LoadError
+  warn '"interactive_editor" not loaded: gem install interactive_editor'
+end
