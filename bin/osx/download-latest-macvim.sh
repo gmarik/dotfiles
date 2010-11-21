eed7f9e8078d4080e91c6c11e13c1c38cb440cee
gmarik <gmarik@gmail.com>
2010-11-21 00:13:25 -0600

    Bash: fixed script download macvim snapshot

--- bin/osx/download-latest-macvim.sh
+++ bin/osx/download-latest-macvim.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-download_pattern='<a href="/downloads/b4winckler/macvim/MacVim-snapshot-.*">'
-download_path=$(curl https://github.com/b4winckler/macvim/downloads| grep "${download_pattern}"|cut -f2 -d\"|head -1)
-dirname=$(basename -s tgz ${download_path})
-curl --location https://github.com${download_path} | tar -x -z -C ~/Downloads/ && open ~/Downloads/${dirname}
+download_pattern='<a href="/downloads/b4winckler/macvim/MacVim-snapshot-.*">' \
+download_path=$(curl https://github.com/b4winckler/macvim/downloads| grep "${download_pattern}"|cut -f2 -d\"|head -1) \
+download_dir=~/Downloads/$(basename -s .tbz ${download_path})
+echo "---------------------------------------------"
+echo "downloading $download_path into $download_dir"
+echo "---------------------------------------------"
+curl --location https://github.com${download_path} | tar -x -z -C ~/Downloads/ && open ${download_dir}
