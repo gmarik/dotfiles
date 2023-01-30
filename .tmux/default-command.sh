002dc729defa9f6ce8d45a717c0a11f287e97215
gmarik <gmarik+git@gmail.com>
2023-01-29 20:05:27 -0500

    Tmux: minor default-command impovement
    - use case swtich instead just if

--- .tmux/default-command.sh
+++ .tmux/default-command.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 
-user_shell=$SHELL
+user_shell="$SHELL"
 
 command_exists () {
   type "$1" &> /dev/null ;
 }
 
-system_name="`uname -s`"
-if [ "$system_name" == "Darwin" ]; then
-  if command_exists reattach-to-user-namespace; then
-    # reattach-to-user-namespace -l sh ~/dotfiles/tmux/clipboard.sh &
-    reattach-to-user-namespace -l "$user_shell"
-  else
-    echo "reattach-to-user-namespace not found \
-      $system_name clipboard will not work \
-      you can: brew install reattach-to-user-namespace"
-    $user_shell
-  fi
-else
-  $user_shell
-fi
+
+platform="`uname -s|tr '[A-Z]' '[a-z]'`"
+
+case "${platform}" in
+  darwin) 
+	if command_exists reattach-to-user-namespace; then
+	  # reattach-to-user-namespace -l sh ~/dotfiles/tmux/clipboard.sh &
+	  reattach-to-user-namespace -l "$user_shell"
+	else
+	  echo "reattach-to-user-namespace not found \
+	    $platform clipboard will not work \
+	    you can: brew install reattach-to-user-namespace"
+	  $user_shell
+	fi
+  linux)
+	exec $user_shell;;
+  *)
+	exec $user_shell;;
+esac
