3deb76ab484243afb26c55b73be0f83690425788
gmarik <gmarik+git@gmail.com>
2023-01-29 23:03:20 -0500

    Tmux: syntax error and formattig
    - default command missed ;; for darwin case
    - as well as format syntax

--- .tmux/default-command.sh
+++ .tmux/default-command.sh
@@ -11,17 +11,19 @@ platform="`uname -s|tr '[A-Z]' '[a-z]'`"
 
 case "${platform}" in
   darwin) 
-	if command_exists reattach-to-user-namespace; then
-	  # reattach-to-user-namespace -l sh ~/dotfiles/tmux/clipboard.sh &
-	  reattach-to-user-namespace -l "$user_shell"
-	else
-	  echo "reattach-to-user-namespace not found \
-	    $platform clipboard will not work \
-	    you can: brew install reattach-to-user-namespace"
-	  $user_shell
-	fi
+    if command_exists reattach-to-user-namespace; then
+      # reattach-to-user-namespace -l sh ~/dotfiles/tmux/clipboard.sh &
+      reattach-to-user-namespace -l "$user_shell"
+    else
+      echo "reattach-to-user-namespace not found \
+        $platform clipboard will not work \
+        you can: brew install reattach-to-user-namespace"
+
+      exec $user_shell
+    fi
+    ;;
   linux)
-	exec $user_shell;;
+    exec $user_shell;;
   *)
-	exec $user_shell;;
+    exec $user_shell;;
 esac
